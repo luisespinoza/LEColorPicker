e211b1de99e8db71064638d48353bcb7b1d3ac86
Luis Espinoza <luis.espinoza.severino@gmail.com>
2012-12-02 23:18:54 -0300

    Initial Commit

---
+++ LEColorPicker/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  LEColorPicker
+//
+//  Created by Luis Enrique Espinoza Severino on 02-12-12.
+//  Copyright (c) 2012 LuisEspinoza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ LEColorPicker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LEColorPicker
+//
+//  Created by Luis Enrique Espinoza Severino on 02-12-12.
+//  Copyright (c) 2012 LuisEspinoza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
