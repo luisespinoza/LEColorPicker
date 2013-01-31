247ba5f4005e6d7bcf3ed130a37ba66c0b138944
Luis Enrique Espinoza Severino <luis.espinoza.severino@gmail.com>
2013-01-30 23:36:44 -0300

    LEColorPickerGPU added

---
+++ LEColorPicker/LEColorPickerGPU/LEColorPickerGPU.h
@@ -0,0 +1,13 @@
+//
+//  LEColorPickerGPU.h
+//  LEColorPickerDemo
+//
+//  Created by Luis Enrique Espinoza Severino on 30-01-13.
+//  Copyright (c) 2013 LuisEspinoza. All rights reserved.
+//
+
+#import "LEColorPicker.h"
+
+@interface LEColorPickerGPU : LEColorPicker
+
+@end
---
+++ LEColorPicker/LEColorPickerGPU/LEColorPickerGPU.m
@@ -0,0 +1,20 @@
+//
+//  LEColorPickerGPU.m
+//  LEColorPickerDemo
+//
+//  Created by Luis Enrique Espinoza Severino on 30-01-13.
+//  Copyright (c) 2013 LuisEspinoza. All rights reserved.
+//
+
+#import "LEColorPickerGPU.h"
+
+@implementation LEColorPickerGPU
+
++ (NSDictionary*)dictionaryWithColorsPickedFromImage:(UIImage *)image
+{
+    return nil;
+}
+
+
+
+@end
