d62132bd3ebd51db14888ea28d4bc9767abe0e2a
Luis Enrique Espinoza Severino <luis.espinoza.severino@gmail.com>
2013-08-03 13:26:04 -0400

    Warning for non ARC compilation added.

--- LEColorPicker/LEColorPicker.m
+++ LEColorPicker/LEColorPicker.m
@@ -6,6 +6,10 @@
 //  Copyright (c) 2012 Luis Espinoza. All rights reserved.
 //
 
+#if ! __has_feature(objc_arc)
+#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+#endif
+
 #import "LEColorPicker.h"
 #import "UIColor+YUVSpace.h"
 #import <OpenGLES/ES2/gl.h>
